1daa1497cb887d3b865690ada6aec3a028d94b13
szymc <szymciu01@o2.pl>
2022-05-10 12:54:20 +0200

    Commit 5.1

--- script.js
+++ script.js
@@ -15,7 +15,20 @@
   })
 
   cw1.addEventListener("click", function () {
-    //TODO implement it
+    answer.innerHTML="Loading...";
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response=>response.json())
+        .then(function (data){
+          appendData(data);
+        })
+    function appendData(data){
+      answer.innerHTML="";
+      for (var i =0; i <data.length;i++){
+        var div = document.createElement("div");
+        div.innerHTML= '<strong>UserID: ' + data[i].userId + '</strong> <strong>id: ' + data[i].id + '</strong> <br> <strong>title: </strong>' + data[i].title + '<br> <strong>body:</strong> '+ data[i].body + '<br><br>';
+        answer.appendChild(div);
+      }
+    }
   })
 
   cw2.addEventListener("click", function () {
